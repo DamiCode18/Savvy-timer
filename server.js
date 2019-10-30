c4d5ad25b91ded8ee92efde0ebba49704d079676
DamiCode18 <adebayoribukunoluwa@yahoo.com>
2019-10-30 14:43:12 +0100

    heroku build update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -15,10 +16,20 @@ const uri = process.env.DB;
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 
 const connection = mongoose.connection;
-connection.once('open', () => {
+connection.once('open', (req, res) => {
 	console.log('Database Successfully Launched!!');
 });
 
-app.listen('port', () => {
+app.use('/', routes);
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
+app.listen(port, () => {
 	console.log(`server running on port: ${port}`);
 });
