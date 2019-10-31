efaaec5cb900c54ba91dab1f2e4999b46ca035b1
DamiCode18 <adebayoribukunoluwa@yahoo.com>
2019-10-31 18:42:49 +0100

    new database commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path');
 
-require('dotenv').config();
+// require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,7 +12,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-const uri = process.env.DB;
+// const uri = process.env.DB;
+const uri = 'mongodb://localhost/savvytimer';
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 
 const connection = mongoose.connection;
