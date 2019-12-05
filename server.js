cf698e0fefe8cf6321ca36ff9e506a0db7e679c6
DamiCode18 <adebayoribukunoluwa@yahoo.com>
2019-12-05 08:10:22 +0100

    heroku commit

--- server.js
+++ server.js
@@ -36,10 +36,10 @@ app.use('/userDetails', userDetailsRouter);
 app.use('/users', userRouter);
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
+	app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+		res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 	});
 }
 
