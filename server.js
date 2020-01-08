d4e7175b3cf19f789a9434b792b4b54e5ae81b79
DamiCode18 <adebayoribukunoluwa@yahoo.com>
2020-01-08 16:33:16 +0100

    mabinu

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ require('./config/passport')(passport);
 
 const uri = require('./config/keys').mongoURI;
 mongoose
-	.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
+	.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: false})
 	.then(() => {
 		console.log('Database Successfully Launched!!');
 	})
