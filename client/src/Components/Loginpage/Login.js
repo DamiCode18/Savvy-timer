93ed74d1fcb9667815a41e35f34529384a28dad7
DamiCode18 <adebayoribukunoluwa@yahoo.com>
2020-02-10 10:14:29 +0100

    alignment on login and register page

--- client/src/Components/Loginpage/Login.js
+++ client/src/Components/Loginpage/Login.js
@@ -5,7 +5,6 @@ import {loginUser} from '../../actions/authActions';
 import './Login.css';
 import {Link} from 'react-router-dom';
 import classnames from 'classnames';
-import logImg from '../../images/man-wear-watches-that-are-connected-their-mobile-devices_10045-380.jpg';
 
 class Login extends Component {
 	state = {
@@ -45,64 +44,61 @@ class Login extends Component {
 	render() {
 		const {errors} = this.state;
 		return (
-			<div className=''>
-				<div className='row mx-auto'>
-					<div className='col-md-5 col-sm-12 text-center mt-5'>
-						<h1 style={{color: '#0d3859', fontWeight: 'bolder'}}>Welcome back,</h1>
-						<p style={{color: '#0d3859', fontWeight: '500'}}>Login to continue</p>
-						{/* <img src={logImg} alt='img' className='logImg' /> */}
-					</div>
-					<div className='p-5 m-auto col-md-7 col-sm-12'>
-						<form className='form' noValidate onSubmit={this.onSubmit}>
-							<div className='form-group mb-4'>
-								<label htmlFor='email'>Email</label>
-								<input
-									value={this.state.email}
-									type='email'
-									id='email'
-									name='email'
-									className={classnames('form-control', {'is-invalid': errors.email})}
-									placeholder='damilare@techsavvyng.com'
-									onChange={this.onChange}
-								/>
-								{errors.email && <div className='invalid-feedback'>{errors.email}</div>}
-							</div>
-							<div className='form-group mb-4'>
-								<label htmlFor='password'>Password</label>
-								<input
-									type='password'
-									id='password'
-									name='password'
-									className={classnames('form-control', {'is-invalid': errors.password})}
-									placeholder='**********'
-									value={this.state.password}
-									onChange={this.onChange}
-								/>
-								{errors.password && <div className='invalid-feedback'>{errors.password}</div>}
-							</div>
-							<button
-								style={{
-									borderRadius : '3px',
-									padding      : '10px',
-									width        : '100%',
-									background   : '#4299E1',
-									color        : '#fff',
-									marginTop    : '15px'
-								}}
-								type='submit'
-								className='btn'
-							>
-								Login
-							</button>
-							<div className='mt-2'>
-								<p>
-									You Don't Have an Account Yet?<Link className='ml-1' to='/register'>
-										Create one now
-									</Link>
-								</p>
-							</div>
-						</form>
-					</div>
+			<div className='row mx-auto container'>
+				<div className='col-md-5 col-sm-12 text-left mt-5'>
+					<h1 style={{color: '#0d3859', fontWeight: 'bolder'}}>Welcome back,</h1>
+					<p style={{color: '#0d3859', fontWeight: '500'}}>Login to continue</p>
+				</div>
+				<div className='p-5 m-auto col-md-7 col-sm-12'>
+					<form className='form' noValidate onSubmit={this.onSubmit}>
+						<div className='form-group mb-4'>
+							<label htmlFor='email'>Email</label>
+							<input
+								value={this.state.email}
+								type='email'
+								id='email'
+								name='email'
+								className={classnames('form-control', {'is-invalid': errors.email})}
+								placeholder='damilare@techsavvyng.com'
+								onChange={this.onChange}
+							/>
+							{errors.email && <div className='invalid-feedback'>{errors.email}</div>}
+						</div>
+						<div className='form-group mb-4'>
+							<label htmlFor='password'>Password</label>
+							<input
+								type='password'
+								id='password'
+								name='password'
+								className={classnames('form-control', {'is-invalid': errors.password})}
+								placeholder='**********'
+								value={this.state.password}
+								onChange={this.onChange}
+							/>
+							{errors.password && <div className='invalid-feedback'>{errors.password}</div>}
+						</div>
+						<button
+							style={{
+								borderRadius : '3px',
+								padding      : '10px',
+								width        : '100%',
+								background   : '#4299E1',
+								color        : '#fff',
+								marginTop    : '15px'
+							}}
+							type='submit'
+							className='btn'
+						>
+							Login
+						</button>
+						<div className='mt-2'>
+							<p>
+								You Don't Have an Account Yet?<Link className='ml-1' to='/register'>
+									Create one now
+								</Link>
+							</p>
+						</div>
+					</form>
 				</div>
 			</div>
 		);
--- client/src/Components/RegisterPage/Register.js
+++ client/src/Components/RegisterPage/Register.js
@@ -44,91 +44,89 @@ class Register extends Component {
 	render() {
 		const {errors} = this.state;
 		return (
-			<div className='register mt-2'>
-				<div className='row mx-auto'>
-					<div className='col-md-5 col-sm-12 text-center mt-5'>
-						<h1 style={{color: '#0d3859', fontWeight: 'bolder'}}>Create an account</h1>
-					</div>
-					<div className='p-5 m-auto col-md-7 col-sm-12'>
-						<form noValidate onSubmit={this.onSubmit} className='form'>
-							<div className='form-row'>
-								<div className='form-group col-md-6 mb-4'>
-									<label htmlFor='firstname'>Firstname</label>
-									<input
-										type='text'
-										id='firstname'
-										name='firstname'
-										className={classnames('form-control mb-2', {'is-invalid': errors.firstname})}
-										placeholder='Dammy'
-										value={this.state.firstname}
-										onChange={this.onChange}
-									/>
-									{errors.firstname && <div className='invalid-feedback'>{errors.firstname}</div>}
-								</div>
-								<div className='form-group col-md-6 mb-4'>
-									<label htmlFor='lastname'>Lastname</label>
-									<input
-										type='text'
-										id='lastname'
-										name='lastname'
-										className={classnames('form-control mb-2', {'is-invalid': errors.lastname})}
-										placeholder='Johnson'
-										value={this.state.lastname}
-										onChange={this.onChange}
-									/>
-									{errors.lastname && <div className='invalid-feedback'>{errors.lastname}</div>}
-								</div>
+			<div className='row mx-auto container'>
+				<div className='col-md-5 col-sm-12 text-left mt-5'>
+					<h1 style={{color: '#0d3859', fontWeight: 'bolder'}}>Create an account</h1>
+				</div>
+				<div className='p-5 m-auto col-md-7 col-sm-12'>
+					<form noValidate onSubmit={this.onSubmit} className='form'>
+						<div className='form-row'>
+							<div className='form-group col-md-6 mb-4'>
+								<label htmlFor='firstname'>Firstname</label>
+								<input
+									type='text'
+									id='firstname'
+									name='firstname'
+									className={classnames('form-control mb-2', {'is-invalid': errors.firstname})}
+									placeholder='Dammy'
+									value={this.state.firstname}
+									onChange={this.onChange}
+								/>
+								{errors.firstname && <div className='invalid-feedback'>{errors.firstname}</div>}
 							</div>
+							<div className='form-group col-md-6 mb-4'>
+								<label htmlFor='lastname'>Lastname</label>
+								<input
+									type='text'
+									id='lastname'
+									name='lastname'
+									className={classnames('form-control mb-2', {'is-invalid': errors.lastname})}
+									placeholder='Johnson'
+									value={this.state.lastname}
+									onChange={this.onChange}
+								/>
+								{errors.lastname && <div className='invalid-feedback'>{errors.lastname}</div>}
+							</div>
+						</div>
 
-							<div className='form-group mb-4'>
-								<label htmlFor='email'>Email</label>
-								<input
-									type='email'
-									id='email'
-									name='email'
-									className={classnames('form-control mb-2', {'is-invalid': errors.email})}
-									placeholder='damilare@techsavvyng.com'
-									value={this.state.email}
-									onChange={this.onChange}
-								/>
-								{errors.email && <div className='invalid-feedback'>{errors.email}</div>}
-							</div>
-							<div className='form-group mb-4'>
-								<label htmlFor='password'>Password</label>
-								<input
-									type='password'
-									id='password'
-									name='password'
-									className={classnames('form-control mb-4', {'is-invalid': errors.password})}
-									placeholder='*************'
-									value={this.state.password}
-									onChange={this.onChange}
-								/>
-								{errors.password && <div className='invalid-feedback'>{errors.password}</div>}
-							</div>
-							<button
-								style={{
-									borderRadius : '3px',
-									padding      : '10px',
-									width        : '100%',
-									background   : '#4299E1',
-									color        : '#fff',
-									marginTop    : '12px'
-								}}
-								type='submit'
-								className='btn btn-info'
-							>
-								Register
-							</button>
-							<div className='mt-2'>
-								<p>
-									Already Have an Account!<Link className='ml-1' to='/login'>
-										Login
-									</Link>
-								</p>
-							</div>
-						</form>
-					</div>
+						<div className='form-group mb-4'>
+							<label htmlFor='email'>Email</label>
+							<input
+								type='email'
+								id='email'
+								name='email'
+								className={classnames('form-control mb-2', {'is-invalid': errors.email})}
+								placeholder='damilare@techsavvyng.com'
+								value={this.state.email}
+								onChange={this.onChange}
+							/>
+							{errors.email && <div className='invalid-feedback'>{errors.email}</div>}
+						</div>
+						<div className='form-group mb-4'>
+							<label htmlFor='password'>Password</label>
+							<input
+								type='password'
+								id='password'
+								name='password'
+								className={classnames('form-control mb-4', {'is-invalid': errors.password})}
+								placeholder='*************'
+								value={this.state.password}
+								onChange={this.onChange}
+							/>
+							{errors.password && <div className='invalid-feedback'>{errors.password}</div>}
+						</div>
+						<button
+							style={{
+								borderRadius : '3px',
+								padding      : '10px',
+								width        : '100%',
+								background   : '#4299E1',
+								color        : '#fff',
+								marginTop    : '12px'
+							}}
+							type='submit'
+							className='btn btn-info'
+						>
+							Register
+						</button>
+						<div className='mt-2'>
+							<p>
+								Already Have an Account!<Link className='ml-1' to='/login'>
+									Login
+								</Link>
+							</p>
+						</div>
+					</form>
 				</div>
 			</div>
 		);
