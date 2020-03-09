63c3e394570f23e488ddb1a5e6cd40507b1fd327
DamiCode18 <adebayoribukunoluwa@yahoo.com>
2020-03-09 12:02:13 +0100

    currentTime detection

--- client/src/Components/User/User.js
+++ client/src/Components/User/User.js
@@ -79,11 +79,11 @@ class User extends Component {
 		let today = new Date();
 		let hourNow = today.getHours();
 		let greeting;
-		if (hourNow > 18) {
+		if (hourNow >= 18) {
 			greeting = 'Good evening!';
-		} else if (hourNow > 12) {
+		} else if (hourNow >= 12) {
 			greeting = ' Good afternoon!';
-		} else if (hourNow > 0) {
+		} else if (hourNow >= 0) {
 			greeting = 'Good morning!';
 		} else {
 			greeting = 'Welcome! ';
